fed5dcc78dafd18685b0def35d09b1ef8e707c0c
Markus Felten <Markus.Felten@gmx.de>
2020-11-02 13:40:52 +0100

    test: wip

--- tests/rate-limit-handler-test.mjs
+++ tests/rate-limit-handler-test.mjs
@@ -49,3 +49,33 @@ test(
   429,
   1000
 );
+
+/*
+const headers = Object.fromEntries(
+  `Content-Encoding: gzip
+Content-Security-Policy: default-src 'none'
+Access-Control-Allow-Origin: *
+Referrer-Policy: origin-when-cross-origin, strict-origin-when-cross-origin
+X-XSS-Protection: 1; mode=block
+Date: Mon, 02 Nov 2020 12:29:34 GMT
+Vary: Accept-Encoding, Accept, X-Requested-With
+Transfer-Encoding: Identity
+Access-Control-Expose-Headers: ETag, Link, Location, Retry-After, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Used, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval, X-GitHub-Media-Type, Deprecation, Sunset
+X-Frame-Options: deny
+Content-Type: application/json; charset=utf-8
+X-Content-Type-Options: nosniff
+Server: GitHub.com
+Strict-Transport-Security: max-age=31536000; includeSubdomains; preload
+Status: 403 Forbidden
+X-RateLimit-Reset: 1604320880
+X-GitHub-Media-Type: github.v3; format=json
+X-RateLimit-Used: 5000
+X-RateLimit-Limit: 5000
+X-RateLimit-Remaining: 0`
+    .split(/\n/)
+    .map(l => {
+      const m = l.match(/(^[^:]+):\s+(.*)/);
+      return [m[1], m[2]];
+    })
+);
+*/
