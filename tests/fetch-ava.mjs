adc265c43776a92e7bc6db9a60ae93840e8f2d72
Markus Felten <Markus.Felten@gmx.de>
2022-12-31 02:03:19 +0100

    test: with global fetch

--- tests/fetch-ava.mjs
+++ tests/fetch-ava.mjs
@@ -2,6 +2,11 @@ import test from "ava";
 import { stateActionHandler } from "fetch-rate-limit-util";
 
 test("api.github.com", async t => {
+  if(!globalThis.fetch) {
+  	const module = await import("node-fetch");
+  	globalThis.fetch = module.default;
+  }
+
   const res = await stateActionHandler(fetch, "https://api.github.com/", {});
   t.true(res.ok);
 });
